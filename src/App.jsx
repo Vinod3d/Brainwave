f60722372fe31307d926e4f53109aa9073d9f485
Vinod Chandra <vinodchandra979@gmail.com>
2024-06-25 18:10:07 +0530

    Brainwave

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Benefits from "./components/Benefits"
+import Button from "./components/Button"
+import Collaboration from "./components/Collaboration"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Pricing from "./components/Pricing"
+import Roadmap from "./components/Roadmap"
+import Section from "./components/Section"
+import Services from "./components/Services"
+function App() {
+
+  return (
+    <>
+      
+      <Header/>
+      <Hero/>
+      <Benefits/>
+      <Collaboration/>
+      <Services/>
+      <Pricing/>
+      <Roadmap/>
+      <Footer/>
+
+
+
+      <ButtonGradient/>
+    </>
+  )
+}
+
+export default App
